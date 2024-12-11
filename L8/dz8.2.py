8858bc7282a8979b4b83d38204202ea406b11bce
WIN10 <321kuzmich123ytube@gmail.com>
2024-12-12 00:38:41 +0200

    1

--- L8/dz8.2.py
+++ L8/dz8.2.py
@@ -2,7 +2,6 @@ def is_palindrome(text):
     cleaned_text = ''.join(char.lower() for char in text if char.isalnum())
     return cleaned_text == cleaned_text[::-1]
 
-# Тести
 assert is_palindrome('A man, a plan, a canal: Panama') == True, 'Test1'
 assert is_palindrome('0P') == False, 'Test2'
 assert is_palindrome('a.') == True, 'Test3'
