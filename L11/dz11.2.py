ceb7cddf481483203a9d8a1b1952071185ce373d
WIN10 <321kuzmich123ytube@gmail.com>
2024-12-22 18:23:40 +0200

    1

--- L11/dz11.2.py
+++ L11/dz11.2.py
@@ -1,8 +1,11 @@
 def generate_cube_numbers(end):
     num = 2
-    while num ** 3 < end:
-        yield num ** 3
-        num += 1  
+    while True:
+        cube = num ** 3
+        if cube > end:
+            return
+        yield cube
+        num += 1
 
 from inspect import isgenerator
 
@@ -10,6 +13,9 @@ gen = generate_cube_numbers(1)
 assert isgenerator(gen) == True, 'Test0'
 assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
 assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
-assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729], '10 у кубі це 1000'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
 
 print('Ok')
+
+
+
